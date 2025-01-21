63942cf79f1962fdad0d856f6a7e0fd6d2bf9fce
winstoncda <olivier.pro.cda@gmail.com>
2025-01-21 10:28:04 +0100

    création des routes pour ajouter et pour récupérer les todos

--- routes/todos.js
+++ routes/todos.js
@@ -1,4 +1,22 @@
 const router = require("express").Router();
 const connection = require("../database/configDB");
 
+router.get("/", (req, res) => {
+  const sql = "SELECT * FROM todos";
+  connection.query(sql, (err, result) => {
+    if (err) throw err;
+    res.status(200).json(result);
+  });
+});
+
+router.post("/addTodo", (req, res) => {
+  console.log(req.body);
+  const { name, done } = req.body;
+  const insertSql = "INSERT INTO todos (name, done) VALUES (?, ?)";
+  connection.query(insertSql, [name, done], (err, result) => {
+    if (err) throw err;
+    res.status(200).json({ message: "todo insérée" });
+  });
+});
+
 module.exports = router;
